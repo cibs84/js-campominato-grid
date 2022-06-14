8eb3bc8c6edade24e1e34f4327341700ac514354
cibs84 <d.tilesi@gmail.com>
2022-06-14 16:14:43 +0200

    Preparatory phase of comments

--- js/script.js
+++ js/script.js
@@ -10,4 +10,12 @@
 // Altrimenti il gioco va avanti a meno di aver raggiunto il numero massimo di tentativi possibili. In questo caso il gioco finisce con un messaggio di vittoria.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha scelto un numero che non era una bomba.
 
+// FASE PREPARATORIA
+// 1 - Chiedo all'utente di scegliere il livello di difficoltà da 1 a 3
+// 2 - Genero 16 numeri casuali (bombe) nel range di numeri del livello di difficoltà scelto: 1 - gameMaxRange -> Level_1: 1-100; Level_2: 1-81; Level_3: 1-49
+// 3 - Creo variabile con i tentativi andati a buon fine = gameMaxRange - numero bombe generate (16)
+
+
+
+
 
